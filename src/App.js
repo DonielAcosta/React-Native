6acf63b7486d70f7e4379f66c774e49906bdf7e3
Doniel acosta <donielacosta1995@gmail.com>
2024-10-30 15:51:55 -0400

    completando los todos y quitandp

--- src/App.js
+++ src/App.js
@@ -17,6 +17,32 @@ function App(){
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos =todos.length;
+  const searchedTodos = todos.filter(
+    (todo) =>{
+      const  todoText=todo.text.toLowerCase();
+      const  searchText=searchValue.toLocaleLowerCase();
+
+    return todoText.includes(searchText)
+  } );
+  const completeTodo =(text) =>{
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed=true;
+    setTodos(newTodos);
+  }
+
+
+  const deleteTodo =(text) =>{
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex,1);
+    setTodos(newTodos);
+  }
+  
   console.log('los usuarios estan buscando ' + searchValue);
   return(
   <>
@@ -26,11 +52,13 @@ function App(){
       setSearchValue={setSearchValue}
     />
     <TodoList>{
-      defaultTodos.map(todo=>(
+      searchedTodos.map(todo=>(
         <TodoItem
           key={todo.text} 
           text={todo.text}
           completed={todo.completed}
+          onComplete={()=> completeTodo(todo.text)}
+          onDelete={()=> deleteTodo(todo.text)}
         />
       ))
     }</TodoList>
